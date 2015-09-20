4d24c041d47fedec6abf62e63912fe68ab5075dc
megaminx <phil-1900@hotmail.com>
2015-09-20 13:36:37 -0400

    ajax post works

--- RealEstateUI/asset/js/PostUser.js
+++ RealEstateUI/asset/js/PostUser.js
@@ -1,23 +1,23 @@
-
-
-
-function postUser()
-{
-    var formData = {pseudonym:"testuser",firstName:"John",lastName:"Bartholomew",email:"johnb@example.com",phoneNumber:"418-666-6666"};
+function postUser() {
+    var formData =JSON.stringify( {
+        "pseudonym": "testuser",
+        "firstName": "John",
+        "lastName": "Bartholomew",
+        "email": "johnb@example.com",
+        "phoneNumber": "418-666-6666"
+    });
 
     $.ajax({
-        url : "localhost:8080/user",
+        url: "http://localhost:8080/user",
         type: "POST",
         contentType: "application/json",
-        data : formData,
-        dataType:"json",
-        success: function()
-        {
-            alert("yay");
+        data: formData,
+        dataType: "json",
+        success: function (data, status) {
+            alert(status);
         },
-        error: function (httpRequest, textStatus, errorThrown)
-        {
-            alert(errorThrown + " fuck");
+        error: function (httpRequest) {
+            alert(httpRequest.responseText + " fuck");
         }
     });
 }
