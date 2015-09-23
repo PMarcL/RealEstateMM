4ac6f3e5db0d329342cd5a8727fd0cd08e6ac906
Guillaume Lambert <glamb93@gmail.com>
2015-09-22 23:37:35 -0400

    added creation date to session

--- RealEstateMM-B2/RealEstateMM-authentication/src/main/java/org/RealEstateMM/authentication/session/Session.java
+++ RealEstateMM-B2/RealEstateMM-authentication/src/main/java/org/RealEstateMM/authentication/session/Session.java
@@ -1,12 +1,16 @@
 package org.RealEstateMM.authentication.session;
 
+import java.util.Date;
+
 public class Session {
 	public final String pseudonym;
 	public final String token;
+	public final Date creationDate;
 
 	public Session(String pseudonym, String token) {
 		this.pseudonym = pseudonym;
 		this.token = token;
+		creationDate = new Date();
 	}
 
 }
