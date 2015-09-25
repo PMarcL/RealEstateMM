6e768722e13be7b5c52460c44a5ef3249290686e
megaminx <phil-1900@hotmail.com>
2015-09-25 15:04:07 -0400

    automatic login upon signup completion. Logout implemented. Very little frontend code needed to complete US1-4. Working backend required to go further

--- RealEstateMM-B2/RealEstateMM-jersey/src/main/java/org/RealEstateMM/jersey/Main.java
+++ RealEstateMM-B2/RealEstateMM-jersey/src/main/java/org/RealEstateMM/jersey/Main.java
@@ -34,7 +34,7 @@ public class Main {
 	private ResourceConfig setUpRessoures() {
 		ResourceConfig resourceConfig = new ResourceConfig();
 
-		resourceConfig.packages("org.RealEstateMM.jersey.resources");
+		resourceConfig.packages("org.RealEstateMM.jersey");
 		resourceConfig.register(JacksonFeature.class);
 
 		return resourceConfig;
---
+++ RealEstateUI/asset/js/LoginCookie.js
@@ -0,0 +1,26 @@
+function LoginCookie() {
+    var COOKIE_NAME = "realestateUser";
+    this.cookie = function findCookie() {
+        return getCookieValue(COOKIE_NAME);
+    }
+
+    this.isUserPresent = function isUserPresent() {
+        if (getCookieValue(COOKIE_NAME) !=null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function getCookieValue(name) {
+        var re = new RegExp(name + "=([^;]+)");
+        var value = re.exec(document.cookie);
+        return (value != null) ? decodeURI(value[1]) : null;
+    }
+
+    this.delete = function deleteCookie() {
+        document.cookie= COOKIE_NAME + "="+getCookieValue(COOKIE_NAME)+"; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+    }
+
+}
