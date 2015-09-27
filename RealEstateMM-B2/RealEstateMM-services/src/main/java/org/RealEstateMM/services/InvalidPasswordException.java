d9562a62c09c11aa7c453c0a24229f1b4e484729
megaminx <phil-1900@hotmail.com>
2015-09-27 15:30:36 -0400

    forgot to commit exception files

---
+++ RealEstateMM-B2/RealEstateMM-services/src/main/java/org/RealEstateMM/services/InvalidPasswordException.java
@@ -0,0 +1,13 @@
+package org.RealEstateMM.services;
+
+public class InvalidPasswordException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String EXCEPTION_MESSAGE = "You specified the wrong password for this user";
+	
+	public InvalidPasswordException()
+	{
+		super(EXCEPTION_MESSAGE);
+	}
+}
