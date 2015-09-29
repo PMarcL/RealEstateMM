f7a62e185eca0b884ccd49790fad17a14d8dfa11
Michaël Blanchet <mblanchet528@gmail.com>
2015-09-28 22:04:48 -0400

    Began to work on class XmlUserAssembler (not finished)

--- RealEstateMM-B2/RealEstateMM-persistence/src/main/java/org/RealEstateMM/persistence/xml/XmlUserAssembler.java
+++ RealEstateMM-B2/RealEstateMM-persistence/src/main/java/org/RealEstateMM/persistence/xml/XmlUserAssembler.java
@@ -4,6 +4,10 @@ import org.RealEstateMM.domain.user.User;
 
 public class XmlUserAssembler {
 
+	public XmlUserAssembler(XmlUserFactory factory) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public XmlUser fromUser(User user) {
 		// TODO Auto-generated method stub
 		return null;
---
+++ RealEstateMM-B2/RealEstateMM-persistence/src/test/java/org/RealEstateMM/persistence/xml/XmlUserAssemblerTest.java
@@ -0,0 +1,5 @@
+package org.RealEstateMM.persistence.xml;
+
+public class XmlUserAssemblerTest {
+
+}
