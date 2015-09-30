23b4eab588b56bee77ec824626f87a7dcb0c200d
megaminx <phil-1900@hotmail.com>
2015-09-30 17:53:33 -0400

    postProperty request works but the server returns 500. will investigate after supper

--- RealEstateUI/asset/js/PostNewProperty.js
+++ RealEstateUI/asset/js/PostNewProperty.js
@@ -1,20 +1,33 @@
+var loginCookie = new LoginCookie();
 
-function postNewProperty()
-{
-	 var formData = {propertyType:"",properTyAddress:"",propertyPrice:"",propertyOwner:"",propertyStatus:""};
+function postNewProperty() {
+
+    var propertyAddress = {
+        "streetNumber": "25",
+        "streetName": "bobbt",
+        "city": "Quebec",
+        "province": "Quebec",
+        "zipCode": "G1C7K7"
+    };
+    var formData = JSON.stringify({
+        "propertyType": "House",
+        "propertyAddressInformations": propertyAddress,
+        "propertyPrice": "20",
+        "propertyOwner": loginCookie.cookie(),
+        "propertyStatus": "On Sale"
+    });
+
 
     $.ajax({
-        url : "localhost:8080/addProperty",
+        url: "http://localhost:8080/property",
         type: "POST",
         contentType: "application/json",
-        data : formData,
-        dataType:"json",
-        success: function()
-        {
+        data: formData,
+        dataType: "json",
+        success: function () {
             alert("yay");
         },
-        error: function (httpRequest, textStatus, errorThrown)
-        {
+        error: function (httpRequest, textStatus, errorThrown) {
             alert(errorThrown + " fuck");
         }
     });
