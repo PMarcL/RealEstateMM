babf3c1d6fc6e0fa33e4504d0af973bef403faea
Michaël Blanchet <mblanchet528@gmail.com>
2015-10-17 14:14:59 -0400

    Pushed forgotten file

---
+++ RealEstateMM-B2/RealEstateMM-jersey/src/main/java/org/RealEstateMM/jersey/resources/LoginResponse.java
@@ -0,0 +1,35 @@
+package org.RealEstateMM.jersey.resources;
+
+public class LoginResponse {
+
+	private String userType;
+	private String token;
+
+	public LoginResponse() {
+	}
+
+	public LoginResponse(String userType, String token) {
+		this.userType = userType;
+		this.token = token;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean equals(LoginResponse other) {
+		return (this.token.equals(other.token)) && (this.userType.equals(other.userType));
+	}
+}
