64a0251811f973f94a71aac887dc1b58620dc54d
Guillaume Lambert <glamb93@gmail.com>
2015-11-30 12:35:21 -0500

    template method back

--- RealEstateMM-B2/RealEstateMM-context/src/main/java/org/RealEstateMM/context/demo/DemoContext.java
+++ RealEstateMM-B2/RealEstateMM-context/src/main/java/org/RealEstateMM/context/demo/DemoContext.java
@@ -2,18 +2,26 @@ package org.RealEstateMM.context.demo;
 
 import org.RealEstateMM.context.Context;
 
-public class DemoContext implements Context {
+public class DemoContext extends Context {
 	private static final String BASE_URL = "http://localhost:8080/";
 
-	public void apply() {
-		// Careful: The Order is important for dependencies
-		// TODO
+	@Override
+	protected void registerRepositories() {
 		new RepositoryRegisterer().register();
-		new DomainHandlerRegisterer(BASE_URL).register();
-
-		new ServiceRegisterer().register();
-
-		new DataInjector().injectData();
 	}
 
+	@Override
+	protected void registerDomainHandlers() {
+		new DomainHandlerRegisterer(BASE_URL).register();
+	}
+
+	@Override
+	protected void registerServices() {
+		new ServiceRegisterer().register();
+	}
+
+	@Override
+	protected void injectData() {
+		new DataInjector().injectData();
+	}
 }
